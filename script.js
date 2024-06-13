4326cd270c0ebe35f95915045c069cd59e297a5a
dig0w <67347822+dig0w@users.noreply.github.com>
2024-06-13 18:25:05 +0100

    main

---
+++ script.js
@@ -0,0 +1,47 @@
+function focusImage(elem, clss) {
+    elem.classList.add(clss);
+
+    let rclss = "";
+    if (clss == "focusLeft") {
+        rclss = "focusRight";
+    } else {
+        rclss = "focusLeft"
+    };
+
+    setTimeout(() => {
+        if (elem.nextElementSibling) {
+            elem.nextElementSibling.classList.remove(rclss);
+        } else if (elem.previousElementSibling) {
+            elem.previousElementSibling.classList.remove(rclss);
+        };
+    }, 500);
+};
+
+function showSlides(n, i) {
+    let slideIndex;
+
+    const slideshow = document.querySelectorAll("div.slideshow")[i];
+    const slides = [];
+
+    for (let j = 0; j < slideshow.children.length; j++) {
+        if (slideshow.children[j].tagName == "IMG") {
+            slides.push(slideshow.children[j]);
+        };
+    };
+    for (let k = 0; k < slides.length; k++) {
+        if (slides[k].style.display != "none") {
+            slideIndex = k;
+        };
+    };
+
+    slides[slideIndex].style.display = "none";
+
+    slideIndex += n;
+        if (slideIndex > slides.length - 1) {
+            slideIndex = 0;
+        } else if (slideIndex < 0) {
+            slideIndex = slides.length - 1;
+        };
+
+    slides[slideIndex].style.display = "block";
+}
