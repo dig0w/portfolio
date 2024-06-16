165f358f816d8a593732c80877f82abddddf8b5c
dig0w <67347822+dig0w@users.noreply.github.com>
2024-06-17 00:12:25 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function scrollFn(id) {
+    document.querySelector(id).scrollIntoView({ behavior: "smooth", block: "start" });
+};
+
 // Image back to front
 function focusImage(elem, clss) {
     elem.classList.add(clss);
@@ -31,12 +35,12 @@ function showSlides(n, i) {
         };
     };
     for (let k = 0; k < slides.length; k++) {
-        if (slides[k].style.display != "none") {
+        if (slides[k].classList[0] == "hidden") {
             slideIndex = k;
         };
     };
 
-    slides[slideIndex].style.display = "none";
+    slides[slideIndex].classList.remove("hidden");
 
     slideIndex += n;
         if (slideIndex > slides.length - 1) {
@@ -45,5 +49,20 @@ function showSlides(n, i) {
             slideIndex = slides.length - 1;
         };
 
-    slides[slideIndex].style.display = "block";
-}
+    slides[slideIndex].classList.add("hidden");
+};
+
+// On Down Scroll Hide Nav Bar
+let lastScrollY = 0;
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector("header div");
+
+    const currentScrollY = window.scrollY;
+        if (currentScrollY > lastScrollY) {
+            navbar.classList.add("hidden");
+        } else {
+            navbar.classList.remove("hidden");
+        };
+
+    lastScrollY = currentScrollY;
+});
